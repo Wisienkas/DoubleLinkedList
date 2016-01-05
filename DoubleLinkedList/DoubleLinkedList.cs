493a61c4e9779277aafc66cb808eeebbfe2c20ed
Nikolaj Reibke <Nikolaj Reibke>
2016-01-06 00:39:23 +0100

    code

---
+++ DoubleLinkedList/DoubleLinkedList.cs
@@ -0,0 +1,109 @@
+﻿using System;
+
+namespace LinkedList
+{
+    public class DoubleLinkedList<T> where T : IComparable
+    {
+        public ListItem<T> Current { get; internal set; }
+        public ListItem<T> Last { get; internal set; }
+        public ListItem<T> First { get; internal set; }
+
+        public static DoubleLinkedList<T> operator +(DoubleLinkedList<T> list, T value)
+        {
+            if (value == null)
+            {
+                throw new ArgumentNullException();
+            }
+            if(list.First == null)
+            {
+                list.First = new ListItem<T>(value);
+                list.Current = list.First;
+                list.Last = list.First;
+            }
+            else
+            {
+                list.Last.Append(new ListItem<T>(value));
+                list.Last = list.Last.Last();
+            }
+
+            return list;
+        }
+
+        public static bool operator ==(DoubleLinkedList<T> ListA, DoubleLinkedList<T> ListB)
+        {
+            ListItem<T> a = ListA.First;
+            ListItem<T> b = ListB.First;
+            
+            while (true)
+            {                
+                if(a == null || b == null)
+                {
+                    return a == null && b == null;
+                }
+                else if(!(a.Value.Equals(b.Value)))
+                {
+                    return false;
+                }
+
+                a = a.Successor;
+                b = b.Successor;
+            }
+        }
+
+        public static bool operator !=(DoubleLinkedList<T> ListA, DoubleLinkedList<T> ListB)
+        {
+            return !(ListA == ListB);
+        }
+
+        public ListItem<T> this[int key]
+        {
+            get
+            {
+                if(key < 0)
+                {
+                    throw new IndexOutOfRangeException();
+                }
+
+                ListItem<T> cur = First;
+
+                for(int i = 0; i < key; i++)
+                {
+                    if(cur.Successor != null)
+                    {
+                        cur = cur.Successor;
+                    }
+                    else
+                    {
+                        throw new IndexOutOfRangeException();
+                    }
+                }
+                return cur;
+            }
+        }
+    }
+
+    public class ListItem<T>
+    {
+        public ListItem<T> Predecessor { get; internal set; }
+        public ListItem<T> Successor { get; internal set; }
+        public T Value { get; internal set; }
+
+        public ListItem(T value)
+        {
+            Value = value;
+            Predecessor = null;
+            Successor = null;
+        }
+
+        internal void Append(ListItem<T> listItem)
+        {
+            listItem.Predecessor = this;
+            Successor = listItem;
+        }
+
+        internal ListItem<T> Last()
+        {
+            return Successor == null ? this : Successor.Last();
+        }
+    }
+}
---
+++ DoubleLinkedListTest/DoubleLinkedListUnitTest.cs
@@ -0,0 +1,184 @@
+﻿using LinkedList;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace DoubleLinkedListTest
+{
+    [TestClass]
+    public class DoubleLinkedListUnitTest
+    {
+        private DoubleLinkedList<double> List;
+
+        /// <summary>
+        /// This will setup the most basic structure of DoubleLinkedList
+        /// Additionally it serves the purpose to verify that the constructor
+        /// do not fail
+        /// </summary>
+        [TestInitialize]
+        public void Setup()
+        {
+            List = new DoubleLinkedList<double>();
+        }
+
+        /// <summary>
+        /// Inserts, When retrieve Current and assert they are equal
+        /// </summary>
+        [TestMethod]
+        public void TestInsertRetrieveCurrent()
+        {
+            List += 13;
+            Assert.AreEqual(13, List.Current.Value);
+        }
+
+        /// <summary>
+        /// Inserts, When retrieve First and assert they are equal
+        /// </summary>
+        [TestMethod]
+        public void TestInsertRetrieveFirst()
+        {
+            List += 13;
+            Assert.AreEqual(13, List.First.Value);
+        }
+
+        /// <summary>
+        /// Inserts, When retrieve Last and assert they are equal
+        /// </summary>
+        [TestMethod]
+        public void TestInsertRetrieveLast()
+        {
+            List += 13;
+            Assert.AreEqual(13, List.Last.Value);
+        }
+
+        /// <summary>
+        /// Test If inserting multiple works
+        /// Assert for both First, Current and Last
+        /// Expected:
+        ///  - First : 13
+        ///  - Last : 5
+        ///  - Current : 13
+        /// </summary>
+        [TestMethod]
+        public void TestMultipleInsert()
+        {
+            List += 13;
+            List += 1;
+            List += 5;
+            Assert.AreEqual(13, List.First.Value);
+            Assert.AreEqual(13, List.Current.Value);
+            Assert.AreEqual(5, List.Last.Value);
+        }
+
+        [TestMethod]
+        public void TestIndex()
+        {
+            List += 13;
+            List += 1;
+            List += 5;
+            Assert.AreEqual(13, List[0].Value);
+            Assert.AreEqual(1, List[1].Value);
+            Assert.AreEqual(5, List[2].Value);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void TestIndexOutOfBound()
+        {
+            List += 13;
+            List += 1;
+            List += 5;
+            var exception = List[4];
+        }
+
+        [TestMethod]
+        public void TestEqual()
+        {
+            List += 13;
+            List += 1;
+            List += 5;
+
+            DoubleLinkedList<double> List2 = new DoubleLinkedList<double>();
+            List2 += 13;
+            List2 += 1;
+            List2 += 5;
+            
+            Assert.IsTrue(List == List2);
+            Assert.IsFalse(List != List2);
+        }
+
+        [TestMethod]
+        public void TestNotEqualSize()
+        {
+            List += 13;
+            List += 1;
+            List += 5;
+
+            DoubleLinkedList<double> List2 = new DoubleLinkedList<double>();
+            List2 += 13;
+            List2 += 1;
+            List2 += 5;
+            List2 += 5;
+
+            Assert.IsFalse(List == List2);
+            Assert.IsTrue(List != List2);
+
+            List += 5;
+            List += 10;
+
+            Assert.IsFalse(List == List2);
+            Assert.IsTrue(List != List2);
+        }
+
+        [TestMethod]
+        public void TestNotEqualValues()
+        {
+            List += 137;
+            List += 17;
+            List += 57;
+
+            DoubleLinkedList<double> List2 = new DoubleLinkedList<double>();
+            List2 += 13;
+            List2 += 1;
+            List2 += 5;
+
+            Assert.IsFalse(List == List2);
+            Assert.IsTrue(List != List2);
+        }
+
+        [TestMethod]
+        public void TestPredecessor()
+        {
+            List += 5;
+            Assert.IsNull(List.First.Predecessor);
+            List += 3;
+            Assert.IsNotNull(List.Last.Predecessor);
+            Assert.AreEqual(List.First, List.Last.Predecessor);
+        }
+
+        [TestMethod]
+        public void TestSuccessor()
+        {
+            List += 5;
+            List += 3;
+            Assert.AreEqual(3, List.First.Successor.Value);
+            Assert.AreEqual(List.Last.Value, List.First.Successor.Value);
+            Assert.IsNull(List.Last.Successor);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void TestNegativeIndex()
+        {
+            List += 5;
+            var haha = List[-1];
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void TestNullInsertException()
+        {
+            DoubleLinkedList<IComparable> list = new DoubleLinkedList<IComparable>();
+            list += null;
+        }
+    }
+}
